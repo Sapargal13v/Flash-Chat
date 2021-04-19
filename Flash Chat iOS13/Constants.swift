0f659dbada17614ff2f09f64d63bca763eb37148
sapargal13v <sapargal13v@mail.ru>
2021-04-19 21:59:16 +0600

    initial commit

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Hjaseyyh Grursht on 13.04.2021.
+//  Copyright © 2021 almat sapargaliyev. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let appName = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Hjaseyyh Grursht on 14.04.2021.
+//  Copyright © 2021 almat sapargaliyev. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
